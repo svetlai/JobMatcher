afe0c4222663fd7b6ccc87a56417983ba4b87a2e
s i <svetla_22@yahoo.com>
2016-01-24 18:09:24 +0200

    Basic skeleton for iOS app project added.

---
+++ JobMatcher.iOS/JobMatcher.iOS/Data/HttpRequester.m
@@ -0,0 +1,13 @@
+//
+//  HttpRequester.m
+//  JobMatcher.iOS
+//
+//  Created by s i on 1/24/16.
+//  Copyright © 2016 svetlai. All rights reserved.
+//
+
+#import "HttpRequester.h"
+
+@implementation HttpRequester
+
+@end
