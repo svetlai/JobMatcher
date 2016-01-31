ede042a09a124c10d1f2ac5493f56931e7acc7ae
s i <svetla_22@yahoo.com>
2016-01-31 18:29:01 +0200

    Job Seeker Profile edited with views for all sections. Custom cell views added for skills, education and experience. View models edited to obtain data.

---
+++ JobMatcher.iOS/JobMatcher.iOS/SummaryViewController.h
@@ -0,0 +1,13 @@
+//
+//  SummaryViewController.h
+//  JobMatcher.iOS
+//
+//  Created by s i on 1/31/16.
+//  Copyright © 2016 svetlai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SummaryViewController : UIViewController
+
+@end
