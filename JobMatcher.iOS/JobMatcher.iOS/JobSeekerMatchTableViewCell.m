c1b3bfb290497b59e34727682671934cdac83ed8
s i <svetla_22@yahoo.com>
2016-02-03 19:08:36 +0200

    Job seeker matches table added with custom cell.

---
+++ JobMatcher.iOS/JobMatcher.iOS/JobSeekerMatchTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  JobSeekerMatchTableViewCell.m
+//  JobMatcher.iOS
+//
+//  Created by s i on 2/3/16.
+//  Copyright © 2016 svetlai. All rights reserved.
+//
+
+#import "JobSeekerMatchTableViewCell.h"
+
+@implementation JobSeekerMatchTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ JobMatcher.iOS/JobMatcher.iOS/ViewControllers/JobSeeker/JobSeekerMatchesViewController.h
@@ -0,0 +1,13 @@
+//
+//  JobSeekerMatchesViewController.h
+//  JobMatcher.iOS
+//
+//  Created by s i on 2/2/16.
+//  Copyright © 2016 svetlai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JobSeekerMatchesViewController : UIViewController
+
+@end
