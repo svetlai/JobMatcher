de4309ead047ccf42cb98bfec7c290ec33e34fe5
s i <svetla_22@yahoo.com>
2016-02-05 19:36:46 +0200

    Recruiter matches added with custom table cell plus link to messages.

---
+++ JobMatcher.iOS/JobMatcher.iOS/RecruiterMatchTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  RecruiterMatchTableViewCell.m
+//  JobMatcher.iOS
+//
+//  Created by s i on 2/5/16.
+//  Copyright © 2016 svetlai. All rights reserved.
+//
+
+#import "RecruiterMatchTableViewCell.h"
+
+@implementation RecruiterMatchTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ JobMatcher.iOS/JobMatcher.iOS/ViewControllers/Recruiter/RecruiterMatchesViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecruiterMatchesViewController.h
+//  JobMatcher.iOS
+//
+//  Created by s i on 2/5/16.
+//  Copyright © 2016 svetlai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecruiterMatchesViewController : UIViewController
+
+@end
