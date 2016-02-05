c18123a70d4b56835ecb9730547cbc188402c809
s i <svetla_22@yahoo.com>
2016-02-05 15:10:01 +0200

    Messages view controller added with custom cells.

---
+++ JobMatcher.iOS/JobMatcher.iOS/RecruiterMessageTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  RecruiterMessageTableViewCell.m
+//  JobMatcher.iOS
+//
+//  Created by s i on 2/4/16.
+//  Copyright © 2016 svetlai. All rights reserved.
+//
+
+#import "RecruiterMessageTableViewCell.h"
+
+@implementation RecruiterMessageTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
